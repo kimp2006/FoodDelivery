681233f54b6438f8252ffab9927d6b005a889615
kimp2006 <80548633+kimp2006@users.noreply.github.com>
2022-06-01 21:26:43 +0200

    update2

--- app/src/main/java/com/example/fooddelivery/ui/cart/CartFragment.kt
+++ app/src/main/java/com/example/fooddelivery/ui/cart/CartFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fooddelivery.ui.home
+package com.example.fooddelivery.ui.cart
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.fooddelivery.databinding.FragmentHomeBinding
+import com.example.fooddelivery.databinding.FragmentCartBinding
 
-class HomeFragment : Fragment() {
+class CartFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentCartBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(CartViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textCart
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/fooddelivery/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/fooddelivery/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fooddelivery.ui.dashboard
+package com.example.fooddelivery.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.fooddelivery.databinding.FragmentDashboardBinding
+import com.example.fooddelivery.databinding.FragmentProfileBinding
 
-class DashboardFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,13 +22,13 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val dashboardViewModel: ProfileViewModel
+        dashboardViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textProfile
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
